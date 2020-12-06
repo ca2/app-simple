f77f19b9b9ea24d2eb3d4e09282734872e04bd51
Camilo Sasuke Tbs_ <camilosasuke@ca2.cc>
2020-12-06 20:49:09 -0300

    🌌🌍 Dec2020/ca2/C++/Windows : USB Capture Video
    <3tbs, Mummi and bilbo!!
    
    Thomas ONE-MAN ABSOLUTE <3!!
    
    Mummi late dawn (till 6:10AM Oct5) presence contribution!!
    
    bilbo requesting food and to be together (5:10AM Oct5) contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    boflux (Spoofh, Benjamin Kuhl) chatting contribution!!
    
    fpga_guru (vue_equalizer, Oliver Pohl) chatting contribution!!
    
    cmgriffing streaming contribution!!
    
    TimBeaudet (Friends: FletcherLabs, tsjost and Jabokoe) streaming contribution!!
    
    Stumpen_nicklas_dk, sodapopping and EduardoRFS streaming contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    kissloryshy chatting contribution!!
    
    blackjekko from Padova Italia through twitch C++/ca2 interest contribution!!
    
    boflux (Ben, Spoofh, from Germany) chatting contribution!!
    
    parrot_rl chatting contribution (from New Jersey)!!
    
    JPCdk streaming contribution!!
    
    whyyyyyyysoserious streaming chess contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch)  C++/ca2 interest contribution!!
    
    SovereignDev with Unreal streaming contribution!!
    
    Myrkee, Ash_F0x and TimBeaudet streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe (till 18:10? Oct5), Endesga and Fearitself streaming contribution!!
    
    tomrandall (till 19:10 Oct5) streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Goldbargames streaming contribution!!
    
    bytakos streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    erald_guri chatting contribution!!
    
    lasteveq4 C++ interest chat contriubtion!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    oijtx streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    togglebit, GexYT and GoPirateSoftware streaming contribution!!
    
    Jaxstyle, taiyoinoue, RetroMMO, OfficialAndyPyro and david_joffe streaming contribution!!
    
    Tjienta streaming contribution!!
    
    -- hi5 contribution...!!
    
    from eddie machine running Windows 10 Pro remotely from bilbo machine running Windows 10 Pro!!

---
+++ video/video_input/TestVideoInput.cpp
@@ -0,0 +1,131 @@
+// TestvideoInput.cpp: „€„„‚„u„t„u„|„‘„u„„ „„„€„‰„{„… „r„‡„€„t„p „t„|„‘ „{„€„~„ƒ„€„|„„~„€„s„€ „„‚„y„|„€„w„u„~„y„‘.
+//
+
+//#include "stdafx.h"
+#include <windows.h>
+#include "videoInput.h"
+//#include "highgui.h"
+
+#pragma comment(lib, "lib\\opencv\\Release\\opencv_highgui242.lib")
+#pragma comment(lib, "lib\\opencv\\Release\\opencv_core242.lib")
+
+#pragma comment(lib, "videoInput.lib")
+
+void StopEvent(int deviceID, void * userData)
+{
+   videoInput * VI = &videoInput::getInstance();
+
+   VI->closeDevice(deviceID);
+}
+
+int _tmain(int argc, TCHAR * argv[])
+{
+   videoInput * VI = &videoInput::getInstance();
+
+   int i = VI->listDevices();
+
+   if (i > 0)
+   {
+      if (VI->setupDevice(i - 1, 640, 480, 60))
+      {
+         VI->setEmergencyStopEvent(i - 1, NULL, StopEvent);
+
+         if (VI->isFrameNew(i - 1))
+         {
+            int countLeftFrames = 0;
+
+            cvNamedWindow("VideoTest", CV_WINDOW_AUTOSIZE);
+            CvSize size = cvSize(VI->getWidth(i - 1), VI->getHeight(i - 1));
+
+            IplImage * frame;
+
+            frame = cvCreateImage(size, 8, 3);
+
+            while (1)
+            {
+               if (VI->isFrameNew(i - 1))
+               {
+                  VI->getPixels(i - 1, (unsigned char *)frame->imageData);
+
+                  cvShowImage("VideoTest", frame);
+
+                  countLeftFrames = 0;
+               }
+               else
+                  countLeftFrames++;
+
+               char c = cvWaitKey(33);
+
+               if (c == 27)
+                  break;
+
+               if (c == 49)
+               {
+                  CamParametrs CP = VI->getParametrs(i - 1);
+                  CP.Brightness.CurrentValue = 128;
+                  CP.Brightness.Flag = 1;
+                  VI->setParametrs(i - 1, CP);
+               }
+
+               if (!VI->isDeviceSetup(i - 1))
+               {
+                  break;
+               }
+
+               if (countLeftFrames > 60)
+                  break;
+            }
+
+            VI->closeDevice(i - 1);
+
+            cvDestroyWindow("VideoTest");
+         }
+      }
+   }
+
+   if (VI->setupDevice(i - 1, 1920, 1080, 60))
+   {
+      if (VI->isFrameNew(i - 1))
+      {
+         int countLeftFrames = 0;
+
+         cvNamedWindow("VideoTest1", CV_WINDOW_AUTOSIZE);
+         CvSize size = cvSize(VI->getWidth(i - 1), VI->getHeight(i - 1));
+
+         IplImage * frame;
+
+         frame = cvCreateImage(size, 8, 3);
+
+         while (1)
+         {
+            if (VI->isFrameNew(i - 1))
+            {
+               VI->getPixels(i - 1, (unsigned char *)frame->imageData, false);
+               cvShowImage("VideoTest1", frame);
+               countLeftFrames = 0;
+            }
+            else
+               countLeftFrames++;
+
+            char c = cvWaitKey(33);
+
+            if (c == 27)
+               break;
+
+            if (!VI->isDeviceSetup(i - 1))
+            {
+               break;
+            }
+
+            if (countLeftFrames > 60)
+               break;
+         }
+
+         VI->closeDevice(i - 1);
+
+         cvDestroyWindow("VideoTest1");
+      }
+
+   }
+   return 0;
+}
